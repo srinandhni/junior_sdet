75ce6f9134f61eecb7d7c7a2961b72d3dfb4ac92
srinandhni <srinandhini1207@gmail.com>
2025-09-27 19:43:27 +0530

    Update greeting

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -3,6 +3,6 @@ import org.junit.Test;
 public class HelloWorldTest {
     @Test
     public void printHelloWorld() {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! This is my first project!");
     }
 }
